58ee6afdc9195f1195bf6a80ad0e991836ed0f5f
amxr <cosmiclover2003.com>
2025-09-09 11:38:45 +0530

    First Saved files

---
+++ main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './PROJECTS/MAJOR_PROJECT/index.css' // ✅ Correct path & case
+//import App from './useRef/App.jsx' // ✅ Correct path & case
+import App from './App.jsx'
+//import './useContext/index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+      <App />
+  </StrictMode>
+ 
+)
